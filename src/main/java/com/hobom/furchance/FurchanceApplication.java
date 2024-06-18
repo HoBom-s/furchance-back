ed09579ccd072d159825253860ecf9b58e41ab9a
robinyeon <robin.yeon@gmail.com>
2024-06-18 18:42:01 +0900

    first commit towards FurChance

---
+++ src/main/java/com/hobom/furchance/FurchanceApplication.java
@@ -0,0 +1,13 @@
+package com.hobom.furchance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurchanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurchanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hobom/furchance/FurchanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobom.furchance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurchanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
